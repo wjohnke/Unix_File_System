e0759723fabb62e3938fe0695f12f5602254d61d
yzzy3 <yzzy3@tc-m610-login-node623.cobalt.lan>
2018-10-31 16:06:00 -0500

    initial roll-in

---
+++ readme.md
@@ -0,0 +1,11 @@
+Milestone 0: Design
+Due: Monday, 11/5 11:59 PM
+
+Milestone 1: Building Blocks
+Due: Monday, 11/12 11:59 PM
+
+Milestone 2: Directories
+Due: Monday, 11/19 11:59 PM
+
+Milestone 3: Data
+Due: Monday, 12/3 11:59 PM
---
+++ src/F18FS.c
@@ -0,0 +1,17 @@
+#include "dyn_array.h"
+#include "bitmap.h"
+#include "block_store.h"
+#include "F18FS.h"
+
+// you can add more library functions for convenience
+// remember to uncomment the tests for corresponding milestones
+// remember to add pseudo-code for the next milestone
+
+
+
+
+
+
+
+
+
